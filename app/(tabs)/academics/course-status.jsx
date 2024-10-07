d8be1a58dd8ae8b027ffc208433d2d3983e705e9
AdibAhmed317 <ahmedadib317@gmail.com>
2024-10-07 14:47:04 +0600

    package update; expo version update;

--- app/(tabs)/academics/course-status.jsx
+++ app/(tabs)/academics/course-status.jsx
@@ -1,10 +1,49 @@
+/*
+  Course Status Table
+    - Ongoing Courses
+    - Completed Courses
+    - Result Up-Coming
+    - Waiting Course
+    - Previous Failed Courses
+    - Pending/Missing Courses
+*/
+
 import { View, Text } from 'react-native';
 import React from 'react';
+import tw from '@lib/tailwind';
 
 const CourseStatus = () => {
   return (
-    <View>
-      <Text>CourseStatus</Text>
+    <View style={tw`bg-white flex-1 px-4`}>
+      <View style={tw`bg-blue-50 p-2 rounded-xl mt-14`}>
+        <Text>
+          <Text style={tw`font-bold`}>Matric No: </Text>
+          <Text>C123123</Text>
+        </Text>
+        <Text>
+          <Text style={tw`font-bold`}>Semester Name: </Text>
+          <Text>Autumn-2024</Text>
+        </Text>
+        <Text>
+          <Text style={tw`font-bold`}>Registration No: </Text>
+          <Text>0062110005101055</Text>
+        </Text>
+      </View>
+
+      <View style={tw`bg-blue-50 p-2 rounded-xl mt-5`}>
+        <Text>
+          <Text style={tw`font-bold`}>Department: </Text>
+          <Text>Computer Science and Engineering</Text>
+        </Text>
+        <Text>
+          <Text style={tw`font-bold`}>Program: </Text>
+          <Text>Bachelor Program</Text>
+        </Text>
+        <Text>
+          <Text style={tw`font-bold`}>Semester Number: </Text>
+          <Text>Seventh Semester</Text>
+        </Text>
+      </View>
     </View>
   );
 };
